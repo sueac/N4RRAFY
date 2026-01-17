69103b6639eb19a46ff39a4841daa1146d636c6b
Wwwang650 <wwwang650@gmail.com>
2026-01-17 10:04:05 -0500

    func

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 print("hello world")
-#hello
+def QuoteExtraction(Filename):
+    with open(Filename,'r') as file:
+        text=file.read()
+    quotes=[]
+    temp=""
+    for char in text:
+        if char == '"':
+            if inside_quotes:
+                quotes.append(current)
+                current = ""
+                inside_quotes = False
+            else:
+                inside_quotes = True
+        elif inside_quotes:
+            current += char
+
+    return quotes
